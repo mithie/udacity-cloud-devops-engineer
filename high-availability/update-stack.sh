d5b692c307703a135c44839e55d9281973e8146e
Michael Thiem <info@michaelthiem.de>
2020-10-11 22:10:15 +0200

    updated network setup

--- high-availability/update-stack.sh
+++ high-availability/update-stack.sh
@@ -3,7 +3,7 @@
 aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
+--parameters file://$3 \
 --region=eu-central-1
 
-# --parameters file://$3 
 # --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" 
